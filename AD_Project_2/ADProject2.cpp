fe1d3e51c0026ca044702d8fe79408e65e0c8c6f
TonyZ <zyc@gis4.org>
2017-10-04 22:55:39 -0400

    1. Remove MSVS unwanted... 2. Changed to CLang. 3. Init.
    Signed-off-by: TonyZ <zyc@gis4.org>

--- AD_Project_2/ADProject2.cpp
+++ AD_Project_2/ADProject2.cpp
@@ -1,11 +1,15 @@
 // ADProject2.cpp: 定义控制台应用程序的入口点。
 //
 
-#include "stdafx.h"
+#include "common.h"
 
 
 int main()
 {
+    printf("INFSCI 2591 Project 2, YIZ141\n");
+
+
+
     return 0;
 }
 
---
+++ AD_Project_2/common.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <tchar.h>
+#include <math.h>
+#include <tchar.h>
